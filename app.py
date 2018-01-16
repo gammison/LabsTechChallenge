6941fddd71ecc59a3f5ef30c8946608c24a11a3a
Garrison Grogan <garrison.grogan@columbia.edu>
2018-01-16 11:52:02 -0500

    added Garrison's link

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+@app.route('/Garrison', methods=['GET'])
+def main():
+    return render_template('Garrison.html')
+
 
 
 if __name__ == '__main__':
